32de6ae73e2a4d0770375e2078e0499f7822afae
Atta Munnim <dev.munnim.visnext@gmail.com>
2021-09-14 20:51:33 +0500

    complete authentication system

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,6 @@
 class ChefsController< ApplicationController	
-
+	before_action :set_chef, only: [:show, :edit, :update, :destroy]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
 		@chefs = Chef.paginate(page: params[:page], per_page: 5)
@@ -13,6 +14,7 @@ class ChefsController< ApplicationController
 		@chef = Chef.new(chef_params)
 
 		if @chef.save
+			session[:chef_id] = @chef.id
 			flash[:success]= "Welcome #{@chef.chefname} to MyRecipes App!"
 			redirect_to chef_path(@chef)
 		else
@@ -22,16 +24,13 @@ class ChefsController< ApplicationController
 	end
 
 	def show
-		@chef =Chef.find(params[:id])
 		@chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
 	end
 
 	def edit
-		@chef = Chef.find(params[:id])
 	end
 
 	def update
-		@chef = Chef.find(params[:id])
 
 		if @chef.update(chef_params)
 			flash[:success] = "Your account was updated successfully"
@@ -43,7 +42,6 @@ class ChefsController< ApplicationController
 	end
 
 	def destroy
-		@chef = Chef.find(params[:id])
 		@chef.destroy
 		flash[:danger]= "chef and all associated recipes have been deleted!"
 		redirect_to chefs_path
@@ -57,6 +55,17 @@ class ChefsController< ApplicationController
 			
 		end
 
+		def set_chef
+			@chef = Chef.find(params[:id])
+			
+		end
+
+		def require_same_user
+			if current_chef != @chef
+				flash[:danger] ="you can only edit or delete your own account"
+				redirect_to chefs_path
+			end
+		end
 
 
 end
