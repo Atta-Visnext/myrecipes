3bf668669f38a29531c01ff4bce881f7d75f242d
Atta Munnim <dev.munnim.visnext@gmail.com>
2021-09-15 16:14:53 +0500

    Complete ingredients resource

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -56,7 +56,7 @@ class RecipesController < ApplicationController
 
 
 		def recipe_params
-			params.require(:recipe).permit(:name, :description)
+			params.require(:recipe).permit(:name, :description, ingredient_ids: [])
 		end
 
 		def require_same_user
